02f3df48420725f9a4ebc544d3cceb000a0da78a
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-25 14:49:57 +0400

    s

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' 'BEGIN{OFS=","} /^(Registrant|Admin|Tech)/{r=$1} /Name:|Organization:|Street:|City:|State\/Province:|Postal Code:|Country:|Phone:|Fax:|Email:/{f=$1;v=$2;if(f=="Street")v=v" ";if(f=="Phone"||f=="Fax"){print r" "f,v;print r" "f" Ext:,";next} print r" "f,v}' > "$1.csv"
+whois $1 | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); if($2!="") print $1 "," $2}' > $1.csv
