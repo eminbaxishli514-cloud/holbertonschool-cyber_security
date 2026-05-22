c15d2d3d888858119ad7eafbca36fe2ce8f66449
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2026-05-22 04:47:03 -0400

    First

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'json'
+require 'uri'
+
+def post_request(url, body_params)
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+
+    request = Net::HTTP::Post.new(uri.path, {
+        'Content-Type' => 'application/json'
+    })
+    request.body = body_params.to_json
+
+    response = http.request(request)
+
+    puts "Response status: #{response.code} #{response.message}"
+    puts "Response body:"
+    parsed_body = JSON.parse(response.body)
+    if parsed_body.empty?
+        puts "{}"
+    else
+        puts JSON.pretty_generate(parsed_body)
+    end
+end
